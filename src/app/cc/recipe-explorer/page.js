6ef0bdfc47dea7532989f195bf5a072325ffe02a
MustafaRavullu <muhammedravullu@gmail.com>
2023-10-21 17:01:19 +0300

    fix: change the source of the food id
    before I used uuid for id of the food but this caused a bug so now I use the co2 emission of the food because edamam does not provide an id for food

--- src/app/cc/recipe-explorer/page.js
+++ src/app/cc/recipe-explorer/page.js
@@ -1,6 +1,5 @@
 import RecipeSearch from "@/components/recipe-search";
 import RecipeCard from "@/components/recipe-card";
-import { v4 as uuidv4 } from "uuid";
 
 async function getRecipes(url) {
   const response = await fetch(url, { cache: "no-store" });
@@ -8,7 +7,7 @@ async function getRecipes(url) {
 }
 
 async function RecipeExplorer({ searchParams }) {
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchParams.q}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}&random=true&field=label&field=image&field=url&field=calories&field=totalTime`;
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchParams.q}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}&random=true&field=label&field=image&field=url&field=calories&field=totalTime&field=totalCO2Emissions`;
   const recipes = await getRecipes(url);
   return (
     <div className="w-full h-full flex justify-start flex-col items-center">
@@ -20,11 +19,10 @@ async function RecipeExplorer({ searchParams }) {
           </p>
         ) : (
           recipes.hits.map((item) => {
-            let id = uuidv4();
             return (
               <RecipeCard
-                key={id}
-                id={id}
+                key={item.recipe.totalCO2Emissions}
+                id={item.recipe.totalCO2Emissions}
                 label={item.recipe.label}
                 image={item.recipe.image}
                 sourceUrl={item.recipe.url}
